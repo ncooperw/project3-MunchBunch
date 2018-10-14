b5407b2731ce0e4180f1dd0213fef9d8257a38b8
Nicole Cooper-White <nwhite1018@gmail.com>
2018-10-14 14:59:16 -0600

    basic CRUD for user-info and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+
+import { CommonService } from './common.service';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserInfoComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpModule, FormsModule,
   ],
-  providers: [],
+  providers: [CommonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
