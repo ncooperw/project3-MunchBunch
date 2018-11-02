2789c204918d4a85e99c8a2e25d36b73185bea9e
Nicole Cooper-White <nwhite1018@gmail.com>
2018-11-01 19:21:24 -0600

    port issue

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var cors = require ('cors')
 
-var PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Initialize Express
 var app = express();
